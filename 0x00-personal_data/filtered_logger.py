0e7516c40dde83d49bf5e20d9b450fa08e717b7c
wuhibe <wuhibetamire710@gmail.com>
2022-05-12 21:04:27 +0300

    typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ module for task 0 """
 import re
+import typing
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: typing.List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ method to filter data """
     for f in fields:
         message = re.sub('{}=.+?{}'.format(f, separator),
