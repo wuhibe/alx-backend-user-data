afa712fa7277795efe3127a00afdc7098405de84
wuhibe <wuhibetamire710@gmail.com>
2022-05-12 22:30:45 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ module for task 0 """
+import logging
 import re
 import typing
 
@@ -12,3 +13,23 @@ def filter_datum(fields: typing.List[str], redaction: str, message: str,
                          '{}={}{}'.format(f, redaction, separator),
                          message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: typing.List[str]):
+        """ initialize method for class """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum """
+        s = filter_datum(self.fields, self.REDACTION,
+                         super(RedactingFormatter, self).format(record),
+                         self.SEPARATOR)
+        return s
