12c9963a26ab660ba87f740fa48801b64901c493
wuhibe <wuhibetamire710@gmail.com>
2022-05-12 21:01:12 +0300

    format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """ method to filter data """
     for f in fields:
-        message = re.sub(f'{f}=.+?{separator}', f'{f}={redaction}{separator}',
+        message = re.sub('{}=.+?{}'.format(f, separator),
+                         '{}={}{}'.format(f, redaction, separator),
                          message)
     return message
