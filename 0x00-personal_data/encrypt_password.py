26203667cdd5f6828a73d01c84800c90c5a7e77b
wuhibe <wuhibetamire710@gmail.com>
2022-05-12 23:12:18 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ def hash_password(password: str) -> bytes:
     """ method to convert plain text password to hash """
     encoded = password.encode()
     return bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ method that checks if a given password is valid """
+    pass_encoded = password.encode()
+    return bcrypt.checkpw(pass_encoded, hashed_password)
