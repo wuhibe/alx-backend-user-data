5a97e44538548fc909179d57bbf46e7924733ffe
wuhibe <wuhibetamire710@gmail.com>
2022-05-16 20:26:15 +0300

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,15 @@ class BasicAuth(Auth):
     """ Basic Authorization Class
     Inheriting from Auth
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """ method to find base64 string in header
+        """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        lst = authorization_header.split(" ")
+        if lst[0] == 'Basic':
+            lst.pop(0)
+            return ' '.join(lst)
+        return None
