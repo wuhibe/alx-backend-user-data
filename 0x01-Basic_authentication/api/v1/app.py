5e469f4a19df8c620913f3af3dc8331f959662b1
wuhibe <wuhibetamire710@gmail.com>
2022-05-16 16:40:02 +0300

    task 1 - move unauth to index.py - modify route to /unauthorized/

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,13 +28,6 @@ def not_found(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.route('/api/v1/unauthorized', strict_slashes=False)
-def unauth():
-    """ test for unauthorized error
-    """
-    return abort(401)
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
