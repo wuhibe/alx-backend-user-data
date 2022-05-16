b09aa25ef7871ab0241936644c38326377ef6a4d
wuhibe <wuhibetamire710@gmail.com>
2022-05-16 20:38:20 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Authorization module
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
             lst.pop(0)
             return ' '.join(lst)
         return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ method to decode str by base64
+        """
+        bah = base64_authorization_header
+        if not bah or type(bah) is not str:
+            return None
+        try:
+            bah = base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            bah = None
+        return bah
