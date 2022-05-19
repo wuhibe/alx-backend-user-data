baa5a2859bdb5b88eea35c1d26501887e331b5de
wuhibe <wuhibetamire710@gmail.com>
2022-05-19 20:32:05 +0300

    (not a and not b)

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,8 +37,8 @@ def filter_request() -> None:
         return
     if not auth.require_auth(request.path, request_list):
         return
-    if not (auth.authorization_header(request) and
-            auth.session_cookie(request)):
+    if (not auth.authorization_header(request) and
+            not auth.session_cookie(request)):
         abort(401)
     request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
