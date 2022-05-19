15fd3ef32a98bd848a0544f0bc52ea71e5443036
wuhibe <wuhibetamire710@gmail.com>
2022-05-19 19:56:35 +0300

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Session Authorization module
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -8,3 +9,13 @@ class SessionAuth(Auth):
     """ Basic Authorization Class
     Inheriting from Auth
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ instance method to create a session
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+        sess_id = str(uuid.uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
