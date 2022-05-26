997a6902cc7b0c9e2c248459e396f9a6ce49dd17
wuhibe <wuhibetamire710@gmail.com>
2022-05-26 23:45:01 +0300

    task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module for a flask app
+"""
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ basic Flask app returning message """
+    return jsonify(message='Bienvenue')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
